d133ce33851cf4eab4e9ed0430ccbe5459b6973d
Matt Dobson <mdobson4@gmail.com>
2013-03-29 11:15:49 -0400

    Updated example with rescue block, and more helpful output.

--- ruby_example.rb
+++ ruby_example.rb
@@ -6,18 +6,21 @@ require 'usergrid_iron'
 # Enter your orgName below — it’s the username you picked when you signed up at apigee.com
 # Keep the appName as “sandbox”: it’s a context we automatically created for you. 
 # It’s completely open by default, but don’t worry, other apps you create are not!    
-usergrid_api = 'http://api.usergrid.com'
-organization = ''
+usergrid_api = 'https://api.usergrid.com'
+organization = 'YOUR APIGEE.COM USERNAME'
 application = 'sandbox'
 
 #Intialize your client here
 application = Usergrid::Application.new "#{usergrid_api}/#{organization}/#{application}"
 
+
 #Use the method create_{entity name} with named arguments for 
 #the properties you want to create
 #The code below will create a book named 'the old man and the sea'
-response = application.create_book name: 'the old man and the sea'
-
-# the response has returned the entity data
-# response.entity wraps it in an easy-to-use object
-book = response.entity
+begin
+	puts "Success! Here is the object we stored; "
+	puts "notice the timestamps and unique id we created for you:"
+	response = application.create_book name: 'the old man and the sea'
+rescue
+	puts "Could not create the book. Did you enter your organization (username) correctly on line 10 of ruby_example.rb?"
+end
